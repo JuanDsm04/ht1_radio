9836525264c2e063f1eeb740075ce48310eab0d0
Diego Flores <floresdiego041@gmail.com>
2024-01-11 15:14:50 -0600

    Adding functionality to switchOnOff method

--- radio1/Main.java
+++ radio1/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author Juan Solis
  * @creationDate 10/01/2024
@@ -5,6 +7,22 @@
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Radio radio = new Radio();
+        System.out.println("Bienvenido");
+        System.out.println("1. Encender o apagar el radio");
+        System.out.print("Elige una de las opciones: ");
+        String opcion = sc.nextLine();
+        switch (opcion) {
+            case "1":
+                radio.switchOnOff();
+                if(radio.isOn()) System.out.println("Radio encendido");
+                else System.out.println("Radio apagado");
+                break;
         
+            default:
+                System.out.println("Ingresa una opción válida");
+                break;
+        }
     }
 }
